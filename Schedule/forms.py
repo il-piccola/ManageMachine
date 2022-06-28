fa1e616d62692a9aba74e4a58d57924af24611c6
il-piccola <il.piccola.estate@gmail.com>
2022-06-28 13:59:00 +0900

    20220628_01

--- Schedule/forms.py
+++ Schedule/forms.py
@@ -1,5 +1,6 @@
 import datetime
 from email.policy import default
+from turtle import filling
 from typing import Iterable
 from django import forms
 from ManageMachine.settings import *
@@ -11,10 +12,10 @@ class SearchForm(forms.Form) :
         for machine in Machine.objects.all() :
             l.append((str(machine.id), machine.name))
         return l
-    machine = forms.ChoiceField(
+    machines = forms.MultipleChoiceField(
         label=CSV_COL_NAME[1],
         choices=getMachineMenu(),
-        widget=forms.Select())
+        widget=forms.CheckboxSelectMultiple())
     date = forms.DateField(
         label='日付',
         widget=forms.DateInput(attrs={"type":"date"})
--- Schedule/views.py
+++ Schedule/views.py
@@ -6,6 +6,49 @@ from plotly.offline import plot
 from ManageMachine.models import *
 from .forms import *
 
+def getTitleAndMsg() :
+    return {
+        'title' : 'Show Machine Schedule',
+        'msg' : '印刷機の予約状況を表示します',
+    }
+
+class MachineSchedule() :
+    machine = None
+    start = None
+    end = None
+    models = None
+    fig = None
+    def __init__(self, machine, start, end) :
+        self.machine = Machine.objects.get(id=int(machine))
+        self.start = convertDateTimeNative(start)
+        self.end = convertDateTimeNative(end)
+        self.models = Schedule.objects.filter(machine=self.machine.id, start__gte=self.start, end__lt=self.end).order_by('start')
+        if (self.models.count() > 0) :
+            self.fig = self.makeGanttChartHtml()
+    def isModelExists(self) :
+        if (self.models.count() > 0) :
+            return True
+        return False
+    def getMachineName(self) :
+        return self.machine.name
+    def makeGanttChartHtml(self) :
+        # plotlyに渡すDataFrameを作成
+        df = read_frame(self.models)
+        df.drop(['id', 'machine'], axis=1, inplace=True)
+        df.rename(columns={'order':PLOTLY_COL_NAME[0], 'start':PLOTLY_COL_NAME[1], 'end':PLOTLY_COL_NAME[2]}, inplace=True)
+        print(df)
+        # plotlyを使用してHTMLを生成
+        fig = express.timeline(
+            df,
+            x_start=PLOTLY_COL_NAME[1],
+            x_end=PLOTLY_COL_NAME[2], 
+            y=PLOTLY_COL_NAME[0], 
+            color=PLOTLY_COL_NAME[0],
+            height=(len(df)*15+150),
+            range_x=[self.start, self.end])
+        fig.update_xaxes(tickformat="%H:%M")
+        return plot(fig, output_type='div', include_plotlyjs=False)
+
 def show(request) :
     params = getTitleAndMsg()
     if (request.method != 'POST') :
@@ -13,14 +56,14 @@ def show(request) :
         return render(request, 'Schedule/show.html', params)
     form = SearchForm(data=request.POST, datetime=datetime.datetime.now())
     if (form.is_valid()) :
-        machine = form.cleaned_data['machine']
+        machines = form.cleaned_data['machines']
         date = form.cleaned_data['date']
         start = datetime.datetime.combine(date, form.cleaned_data['start'])
         end = datetime.datetime.combine(date, form.cleaned_data['end'])
-        models = Schedule.objects.filter(machine=machine, start__gte=start, end__lt=end).order_by('start')
-        params['models'] = models
-        if (models.count() > 0) :
-            params['fig'] = makeGanttChartHtml(models, start, end)
+        machineScheduleList = []
+        for machine in machines :
+            machineScheduleList.append(MachineSchedule(machine, start, end))
+        params['schedules'] = machineScheduleList
     params['form'] = form
     return render(request, 'Schedule/show.html', params)
 
@@ -31,35 +74,6 @@ def showFromOrder(request, order, name) :
     if (schedules.count() <= 0) :
         return redirect('Schedule:show')
     time = fromDawnTillDuskD(convertDateTimeAware(schedules.first().start))
-    s = convertDateTimeNative(time[0])
-    e = convertDateTimeNative(time[1])
-    models = Schedule.objects.filter(machine=machine, start__gte=s, end__lt=e).order_by('start')
-    params['models'] = models
-    if (models.count() > 0) :
-        params['fig'] = makeGanttChartHtml(models, time[0], time[1])
-    params['form'] = SearchForm(initial={'machine':str(machine)}, datetime=time[0])
+    params['schedules'] = [MachineSchedule(str(machine), time[0], time[1])]
+    params['form'] = SearchForm(initial={'machines':[str(machine)]}, datetime=time[0])
     return render(request, 'Schedule/show.html', params)
-
-def getTitleAndMsg() :
-    return {
-        'title' : 'Show Machine Schedule',
-        'msg' : '印刷機の予約状況を表示します',
-    }
-
-def makeGanttChartHtml(models, start, end) :
-    # plotlyに渡すDataFrameを作成
-    df = read_frame(models)
-    df.drop(['id', 'machine'], axis=1, inplace=True)
-    df.rename(columns={'order':PLOTLY_COL_NAME[0], 'start':PLOTLY_COL_NAME[1], 'end':PLOTLY_COL_NAME[2]}, inplace=True)
-    print(df)
-    # plotlyを使用してHTMLを生成
-    fig = express.timeline(
-        df,
-        x_start=PLOTLY_COL_NAME[1],
-        x_end=PLOTLY_COL_NAME[2], 
-        y=PLOTLY_COL_NAME[0], 
-        color=PLOTLY_COL_NAME[0],
-        height=(len(df)*15+150),
-        range_x=[start, end])
-    fig.update_xaxes(tickformat="%H:%M")
-    return plot(fig, output_type='div', include_plotlyjs=False)
